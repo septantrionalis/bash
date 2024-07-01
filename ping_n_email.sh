dda38c6d949a1a95b48d6f67db37786347e83bf8
Ron <rkinney@tdod.org>
2024-07-01 10:51:41 -0600

    Create ping_n_email.sh

---
+++ ping_n_email.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+FILE=/home/pi/apps/check/FILE_LOCK
+
+TARGET=192.168.1.15
+
+if [ ! -f $FILE ]; then
+
+ping -W 30 -c 1 $TARGET > /dev/null
+
+if [ $? -ne 0 ]; then
+
+touch $FILE
+
+echo "server is down"
+
+/usr/sbin/sendmail -t < /home/pi/apps/check/message.txt fi fi 
