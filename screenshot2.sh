8ba84c52aacfe6ac27dd912b7fca15ae87b0b458
Ron <rkinney@tdod.org>
2024-07-01 10:36:27 -0600

    Create screenshot2.sh

---
+++ screenshot2.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [ $# -eq 1 ]; then
+    echo "Sleeping for $1 second(s)..."
+    sleep "$1"
+    grim -g "1921,0 1920x1080" ~/Documents/$(date -d "today" +"%Y%m%d%H%M_%s").png
+else
+    grim -g "1921,0 1920x1080" ~/Documents/$(date -d "today" +"%Y%m%d%H%M_%s").png
+fi
