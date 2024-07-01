d9b660cd05b7714b03b5c1acfdc8d320dccf0883
Ron <rkinney@tdod.org>
2024-07-01 10:52:33 -0600

    Update ping_n_email.sh

--- ping_n_email.sh
+++ ping_n_email.sh
@@ -1,32 +1,12 @@
-
-
 #!/bin/bash
 
 FILE=/home/pi/apps/check/FILE_LOCK
 
-TARGET=192.168.1.15
+TARGET="192.168.1.15 25"
 
 if [ ! -f $FILE ]; then
 
-ping -W 30 -c 1 $TARGET > /dev/null
-
-if [ $? -ne 0 ]; then
-
-sleep 30
-
-ping -W 30 -c 1 $TARGET > /dev/null
-
-if [ $? -ne 0 ]; then
-
-sleep 30
-
-ping -W 30 -c 1 $TARGET > /dev/null
-
-if [ $? -ne 0 ]; then
-
-sleep 30
-
-ping -W 30 -c 1 $TARGET > /dev/null
+nc -z $TARGET > /dev/null
 
 if [ $? -ne 0 ]; then
 
@@ -34,4 +14,4 @@ touch $FILE
 
 echo "server is down"
 
-/usr/sbin/sendmail -t < /home/pi/apps/check/message.txt fi fi fi fi fi 
+/usr/sbin/sendmail -t < /home/pi/apps/check/message.txt fi fi 
