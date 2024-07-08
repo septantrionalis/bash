480ff4169953d267ffa8141b8b08368f0b9c0e39
Ron <rkinney@tdod.org>
2024-07-08 14:28:40 -0600

    fix cheat.sh comment

--- set-byte/cheat.sh
+++ set-byte/cheat.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# replace the first hex value with the second on the specified file.
+# Replace the hexadecimal value at the offset (first number) with the specified value (the second number) at the specified file.
 FILE=DARK01.SAV    
 if [ -f $FILE ]; then
   ./set-byte.py $FILE 0x39FB 0xff
