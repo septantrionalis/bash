018e67aadebc5066672d6e957da98e75f9124b4b
Ron <rkinney@tdod.org>
2024-07-08 14:25:32 -0600

    changes to cheat.sh

--- set-byte/cheat.sh
+++ set-byte/cheat.sh
@@ -3,21 +3,21 @@
 # replace the first hex value with the second on the specified file.
 FILE=DARK01.SAV    
 if [ -f $FILE ]; then
-  ./set-byte.py DARK01.SAV 0x39FB 0xff
+  ./set-byte.py $FILE 0x39FB 0xff
 else
   echo $FILE not found!
 fi
 
 FILE=DARK02.SAV
 if [ -f $FILE ]; then
-  ./set-byte.py DARK02.SAV 0x39FC 0xff
+  ./set-byte.py $FILE 0x39FC 0xff
 else
   echo $FILE not found!
 fi
 
 FILE=DARK03.SAV
 if [ -f $FILE ]; then
-  ./set-byte.py DARK03.SAV 0x39FD 0xff
+  ./set-byte.py $FILE 0x39FD 0xff
 else
   echo $FILE not found!
 fi
