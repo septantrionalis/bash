59c5ce4b6e835e3b9ec944f207d9b15da690a16f
Ron <rkinney@tdod.org>
2024-12-12 16:59:15 -0700

    add comments to log_cleans2.sh

--- amateur_radio/log_cleans2.sh
+++ amateur_radio/log_cleans2.sh
@@ -26,7 +26,9 @@ function displayHelp() {
     echo -e "${CYAN}DESCRIPTION${NOCOLOR}"
     echo "     This script will create a cleansed POTa and WWFF file from a hamrs adif file. It will currently:"
     echo "     - Add MY_POTA_REF:<POTA_PARK> in the comments section of the POTA adif file."
-    echo "     - Add MY_POTA_REF:<WWFF_PARK> in the comments section of the WWFF adif file."
+    echo "     - Add MY_WWFF_REF:<WWFF_PARK> in the comments section of the WWFF adif file."
+    echo "     - Add <my_sig:4>WWFF to the WWFF adif file"
+    echo "     - Add <my_sig_info:xx>PARK_REFERENCE to the WWFF adif file"
     echo ""
     echo "     Input file is $INPUT"
 }
